c6c90534443d7278885977ac86c541d31ea0ab29
John Goode <johnhgoode@gmail.com>
2017-09-08 14:54:10 -0400

    register endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -14,4 +14,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your are now registered! Have fun!`
+  })
+})
+
 app.listen(process.env.PORT || 8081)
