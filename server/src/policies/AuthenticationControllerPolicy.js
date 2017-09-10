eaa2dc461993193f09b9799efe4f7119b8393acf
John Goode <johnhgoode@gmail.com>
2017-09-10 12:18:30 -0400

    introduce vuetify

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -2,6 +2,39 @@ const Joi = require('joi')
 
 module.exports = {
   register (req, res, next) {
-    next()
+    const schema = {
+      email: Joi.string().email(),
+      password: Joi.string().regex(
+        new RegExp('^[a-zA-Z0-9]{8,32}$')
+      )
+    }
+
+    const {error, value} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(400).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password':
+          res.status(400).send({
+            error: `The password provided faild to mathc the following rules:
+            <br>
+            1. It must contain ONLY the following characters: lower case, upper case, and 0-9.
+            <br>
+            2. It must be between 8 and 32 characters in length.
+            `
+          })
+          break
+        default:
+          res.status(400).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
   }
 }
