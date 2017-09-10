a547067abc598031220b03df65d7fd28f9390a48
John Goode <johnhgoode@gmail.com>
2017-09-10 12:57:28 -0400

    more styling

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -2,17 +2,27 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
-        <v-toolbar flat dense class="blue" dark>
+        <v-toolbar flat dense class="blue darken-4" dark>
           <v-toolbar-title><span style="color:white;">Register</span></v-toolbar-title>
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
-          <input type="email" name="email" placeholder="email " v-model="email" />
+          <v-text-field
+                name="email"
+                label="E-mail"
+                id="email"
+                v-model="email"
+              ></v-text-field>
           <br>
-          <input type="password" name="password" placeholder="password" v-model="password" />
+          <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                v-model="password"
+              ></v-text-field>                 
           <br>
           <div class="error" v-html="error"></div>
           <br>
-          <v-btn @click="register">Register</v-btn>
+          <v-btn class="blue darken-3" @click="register">Register</v-btn>
         </div>
       </div>
     </v-flex>
@@ -50,4 +60,5 @@ export default {
   .error {
     color: red;
   }
+
 </style>
